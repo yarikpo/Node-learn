5b6a251bc85787ed5acaf7eb2ef336287e95f61a
yarik_po <yaroslavpopovich04@gmail.com>
2020-01-30 22:02:52 +0200

    +JSON example & HTML example

--- examples/index.js
+++ examples/index.js
@@ -1,9 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
 	console.log('URL: ' + req.url);
-	res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-	res.end('Hello world!');
+	/*res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+	var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
+	myReadShort.pipe(res);*/ // HTML
+
+
+	res.writeHead(200, {'Content-Type': 'application/json; charset=utf8'});
+	var obj = {
+		model: 'Vista',
+		system: 'XP',
+		year: 2000,
+	};
+	res.end(JSON.stringify(obj)); // JSON
 });
 
 server.listen(19090, '127.0.0.1');
